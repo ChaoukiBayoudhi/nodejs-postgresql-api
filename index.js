b2a2082c89c9e2500ce3988e3e379acabffb54c9
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-03-24 11:29:17 +0100

    Second commit

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ app.get('/', (request, response) => {
     response.json({info: 'Node.js, Express, and Postgres API'})
 })
 
-app.get('/all',db.getAllUsers)
+app.get('/users/all',db.getAllUsers)
+app.get('/user/:id',db.getUserById)
+app.post('/users/new',db.addUser)
+app.put('/users/:id',db.updateUser)
+app.delete('/users/delete/:id',db.removeUser)
 
 app.listen(port, ()=>{
     console.log(`application is running on port ${port}.`)
--- queries.js
+++ queries.js
@@ -7,9 +7,10 @@ const pool=new Pool({
     port:5432,
 })
 
+//returns all users if Ok, Exception if not
 const getAllUsers=(request,response) => {
     pool.query(
-        'select * from users order by name ASC',
+        'select * from users order by id ASC',
 (error,results)=>{
     if(error){
        throw error
@@ -18,9 +19,66 @@ const getAllUsers=(request,response) => {
 response.status(200).json(results.rows)
 })
 }
+//returns a user having as id the value provided as path parameter
+//if Ok, Exception if not
+const getUserById=(request,response)=>{
+    const id=parseInt(request.params.id)
 
+    pool.query('select * from users where id = $1',[id],(error,results)=>{
+        if(error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+//add a new user. The user is provided on the body of the request
+//returns 201 as HTTP status if Ok, Exception if not
+const addUser=(request,response) =>{
+    const {name,email}=request.body
 
+    pool.query('insert into users (name, email) values ($1, $2) returning id', [name,email], (error,results)=>{
+        if(error){
+            throw error
+        }
+        response.status(201).send(`User with ID : ${results.rows[0].id} has been successfully added`)
+    })
+}
+//update user using provided id value on the path
+//the new user attributes values are passed on the body of the http request
+//return 200 as http status if Ok, Exception if not
+
+const updateUser = (request,response) =>{
+    const id=parseInt(request.params.id)
+    const {name,email}=request.body
+
+    pool.query('update users set name=$1, email=$2 where id=$3', [name,email,id],
+    (error,results) =>{
+        if(error){
+            throw error
+        }
+        response.status(200).send(`User with id : ${id} has been successfully updated`)
+    })
+}
+
+//remove user with the passed id in the path
+//returns 200 as HTTP status if Ok, Exception if not
+const removeUser = (request,response) => {
+    const id=parseInt(request.params.id)
+
+    pool.query('delete from users where id=$1',[id],
+    (error, results) => {
+        if(error){
+            throw error
+        }
+        response.status(200).send(`User (id=${id}) has been successfully deleted`)
+    }
+    )
+}
 
 module.exports = {
     getAllUsers,
+    getUserById,
+    addUser,
+    updateUser,
+    removeUser,
 }
